9e4869c06f6b169590ea914deb58fa42b7041ebd
lse-mcc <kalesin90@gmail.com>
2016-04-02 18:31:59 +0600

    Token and password is private now. First implementation of command execution.

--- EntryPointRtm.py
+++ EntryPointRtm.py
@@ -2,8 +2,8 @@ import time
 from slackclient import SlackClient
 from slacker import Slacker
 from TeamcityUtils import get_project_names
+from Config import token
 
-token = ""
 
 slack = Slacker(token)
 
@@ -12,21 +12,43 @@ commands = {"bot": 'Somebody call me?',
                             " hope soon i will learn some cool stuff",
             "bot projects": get_project_names()}
 
+real_commands = {"build" : ""}
+parameters = {"branch"}
+
+
+def execute_command(function, parameter):
+    pass
+
+
+def parse_command(api, message_content):
+    print(message_content[0])
+    if message_content[0] == "!":
+        array_of_words = message_content[1:].split()
+        if array_of_words[0] in real_commands:
+            if array_of_words[1] in parameters:
+                execute_command(array_of_words[0], array_of_words[1])
+
 
 def process_message(api, message):
     if "text" in message:
         message_content = message["text"]
-
+        parse_command(api, message_content)
         if message_content in commands:
             api.chat.post_message("#hackaton-bot", commands[message_content], 'bombila')
+
+
 
 sc = SlackClient(token)
 if sc.rtm_connect():
     while True:
         message = sc.rtm_read()
         if len(message) != 0:
-            unpacked = message[0]
-            process_message(slack, unpacked)
+            print(message[0])
+            if "channel" in message[0]:
+                if message[0]["channel"] == "C0XF2E0RY":
+                    unpacked = message[0]
+                    print(unpacked)
+                    process_message(slack, unpacked)
 
         time.sleep(1)
 else:
--- TeamcityUtils.py
+++ TeamcityUtils.py
@@ -1,9 +1,9 @@
 from pyteamcity import TeamCity
-
+from Config import teamcity_creds
 
 def get_project_names():
-    tc = TeamCity("", "", "teamcity.mcc-tomsk.de")
+    tc = TeamCity(teamcity_creds["login"], teamcity_creds["password"], "teamcity.mcc-tomsk.de")
 
-    project_names = [project['name'] for project in tc.get_projects()]
+    project_names = [project['name'] for project in tc.get_projects()["project"]]
 
     return "\n".join(project_names)
