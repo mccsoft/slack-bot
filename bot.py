dec585f875d0ebf36abede88b8eadd1429c1c7e6
serje.bulavsky <hcoder84@gmail.com>
2016-04-03 22:00:54 +0600

    Entry point refactoring

---
+++ bot.py
@@ -0,0 +1,40 @@
+import time
+from slackclient import SlackClient
+from slacker import Slacker
+from commands import commands
+from config_provider import config_provider
+from slack_utils import get_channel_id
+from сommand_worker import parse_command
+
+
+class Bot:
+
+    def __init__(self):
+        self.token = config_provider.token
+        self.bot_name, self.channel_name = config_provider.bot_settings()
+        self.slack = Slacker(self.token)
+
+    def process_message(self, api, message):
+        if "text" in message:
+            message_content = message["text"]
+            parse_command(message_content)
+            if message_content in commands:
+                api.chat.post_message(self.channel_name, commands[message_content], self.bot_name)
+
+    def run(self):
+        sc = SlackClient(self.token)
+
+        if sc.rtm_connect():
+            while True:
+                message = sc.rtm_read()
+                if len(message) != 0:
+                    print(message)
+                    if "channel" in message[0]:
+                        if message[0]["channel"] == get_channel_id(self.channel_name[1:]):
+                            unpacked = message[0]
+                            print(unpacked)
+                            self.process_message(self.slack, unpacked)
+
+                time.sleep(1)
+        else:
+            print("Connection Failed, invalid token?")
